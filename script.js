2d76448cd9809c5f62f478d8cd29f96e8560e73c
Sergey <klopcool@yandex.ru>
2014-09-19 02:31:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,51 @@
 /***
-**** Variant 1 ***
+ * Variant 1 *
 */
+var PopupOne = function() {
 
-var PopupOne = function(options) {
+  $(document).on('click', '[data-popup-id]', onClickTrigger);
+  $(document).on('click', '[data-popup="close"]', onClickClose);
+  $(document).on('click', '[data-popup="layout"]', onClickClose);
   
+  function onClickTrigger(e) {
+    e.preventDefault();
+    var idPopup = $(e.target).data('popup-id');
+    show(idPopup);
+  }
+  
+  function onClickClose(e) {
+    e.preventDefault();
+    close();
+  } 
+  
+  function show(id) {
+    var popup = $('#' + id);
+    popup.show();
+    setCoords(popup);
+    $('[data-popup="layout"]').show(); 
+  }
+  
+  function close() {
+    $('[data-popup="layout"]').hide();
+    $('[data-popup="block"]').hide(); 
+  }
+  
+  function setCoords(elem) {
+    var scrollTop = $(window).scrollTop(),
+        scrollLeft = $(window).scrollLeft(),
+        winHeight = $(window).height(),
+        winWidth = $(window).width(),
+        popupHeight = elem.outerHeight(),
+        popupWidth = elem.outerWidth();
+        
+    var top = (winHeight - popupHeight)/2 + scrollTop,
+        left = (winWidth - popupWidth)/2 + scrollLeft;
+        
+    elem.css({
+      top: top + 'px',
+      left: left + 'px'  
+    });
+  }
 }
 
-var popupOne = new PopupOne({
-  
-});
+var popupOne = new PopupOne();
