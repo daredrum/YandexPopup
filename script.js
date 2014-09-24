4fd78f8a2c790684ecf4e6d95e08c6339712443e
Sergey <klopcool@yandex.ru>
2014-09-25 02:09:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -165,3 +165,136 @@ var popupTwo = new PopupTwo({
   animationSpeed: 1000,
   closeonbackgroundclick: false
 });
+
+/**
+ * Variant 3
+ * @options module options 
+ ** @trigger button to open the popup
+ ** @closer button to close the popup
+ ** @animation type of animation: 'none', 'fade', 'slideup'. default is 'none'
+ ** @animationSpeed speed of animation. it's used only when 'animation' isn't 'none'. default is 400
+ ** @closeonbackgroundclick closure handlers by clicking background: 'true' or 'false'. default is 'true'
+ */
+ 
+ var PopupThree = function(options) {
+  this._template = options.template || false;
+  this._trigger = options.trigger || '.popup-trigger'; 
+  this._closer = options.closer || '.popup-close';  
+  this._animation = options.animation || 'none';
+  this._animationSpeed = options.animationSpeed || 400;
+  this._closeonbackgroundclick = options.closeonbackgroundclick || true;
+  
+  $(document).on('click', this._trigger, this._onClickTrigger.bind(this));
+  if (this._closeonbackgroundclick) {
+      $(document).on('click', '.c-popup_three__layout', this._onClickClose.bind(this));
+  }
+}
+ 
+PopupThree.prototype._onClickTrigger = function(e) {
+    e.preventDefault();
+    if (!this._template) return false;
+    var idPopup = $(e.target).attr('href');
+    if ($(idPopup).size() == 0) { 
+        this._creatPopup(e.target, idPopup);
+    } else {
+        this._show(idPopup);
+    }    
+}
+
+PopupThree.prototype._onClickClose = function(e) {
+  e.preventDefault();
+  this._close();
+} 
+
+PopupThree.prototype._creatPopup = function(elem, id) {
+  var self = this; 
+  var idTmp = $(elem).data('tmp');
+  var idTmpPopup = $(elem).data('tmp-content');
+  var tmpPopup = $(idTmp).html(); 
+  var tmpContentPopup = $(idTmpPopup).html(); 
+  var compiled = _.template(tmpPopup, { content: tmpContentPopup, id: id.slice(1)});
+  $('body').append(compiled);
+  this._show(id);
+}
+
+PopupThree.prototype._show = function(id) {
+  var popup = $(id);
+  popup.css({ top: '-' + popup.outerHeight() + 'px' }).show();
+  this._setCoords(popup);
+  $('.c-popup_three__layout').show();
+  
+  popup.on('click', this._closer, this._onClickClose.bind(this));
+}
+
+PopupThree.prototype._close = function() {
+    switch (this._animation) {
+    case 'fade':
+      $('.c-popup_three').animate({ opacity: 0 }, this.__animationSpeed);
+      $('.c-popup_three__layout').animate({ opacity: 0 }, this.__animationSpeed, this._hide);
+      break;
+    case 'slideup':
+      $('.c-popup_three').animate({
+        top: '-' + $('.c-popup_three').outerHeight() + 'px',
+        opacity: 1 
+      }, this.__animationSpeed, this._hide);
+      $('.c-popup_three__layout').css({ opacity: 1 });
+      break;
+    default:
+      $('.c-popup_three').css({ opacity: 1 });
+      $('.c-popup_three__layout').css({opacity: 1});
+      this._hide();
+  }
+}
+
+PopupThree.prototype._hide = function() {
+  $('.c-popup_three').hide();
+  $('.c-popup_three__layout').hide();
+}
+
+PopupThree.prototype._setCoords = function (elem) {
+  var winHeight = $(window).height(),
+      winWidth = $(window).width(),
+      popupHeight = elem.outerHeight(),
+      popupWidth = elem.outerWidth();
+      
+  var top = (winHeight - popupHeight)/2,
+      left = (winWidth - popupWidth)/2;
+  
+  this._animate(elem, top, left); 
+}
+
+PopupThree.prototype._animate = function(elem, top, left) {
+  switch (this._animation) {
+    case 'fade':
+      elem.css({
+        top: top + 'px',
+        left: left + 'px'  
+      }).animate({opacity: 1}, this.__animationSpeed);
+      $('.c-popup_three__layout').animate({opacity: 1}, this.__animationSpeed);
+      break;
+    case 'slideup':
+      elem.css({
+        left: left + 'px',
+      }).animate({
+        top: top + 'px',
+        opacity: 1
+      }, this.__animationSpeed);
+      $('.c-popup_three__layout').animate({opacity: 1}, this.__animationSpeed);
+      break;
+    default:
+      elem.css({
+        top: top + 'px',
+        left: left + 'px',
+        opacity: 1
+      });
+      $('.c-popup_three__layout').css({opacity: 1});
+  }
+}
+
+var popupThree = new PopupThree({
+  trigger: '.c-popup_three__trigger',
+  closer: '.c-popup__close', 
+  animation: 'slideup',
+  animationSpeed: 1000,
+  closeonbackgroundclick: false  
+});
